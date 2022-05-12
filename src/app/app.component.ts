33569180a6c8f8ebc611ba95ea9a2a39553eb811
vijaykumar <vijay1111mane@gmail.com>
2022-05-12 11:05:24 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'lazy_loading_component';
+}
