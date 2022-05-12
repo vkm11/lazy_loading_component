69864af9d2932230676046748754ff7a18a41a4e
vijaykumar <vijay1111mane@gmail.com>
2022-05-12 12:49:33 +0530

    commit;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'lazy_loading_component';
+  constructor(private viewContainer: ViewContainerRef,
+    private cfr: ComponentFactoryResolver) { }
+
+  async loadAdmin() {
+    this.viewContainer.clear();
+    const { AdminlistComponent } = await import('./adminlist/adminlist.component')
+    this.viewContainer.createComponent(
+      this.cfr.resolveComponentFactory(AdminlistComponent)
+    )
+  }
+  async loadUser(){
+    this.viewContainer.clear();
+    const{ UserlistComponent }=await import('./userlist/userlist.component')
+    this.viewContainer.createComponent(
+      this.cfr.resolveComponentFactory(UserlistComponent)
+    )
+  }
+
 }
